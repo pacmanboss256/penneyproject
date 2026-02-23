c7cfa4f320e6449f9c0e9e2b4fd3c532e05a75de
pacmanboss256 <pacmanboss972@gmail.com>
2026-02-23 13:39:06 -0500

    abstract to four bits

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
 
     x = deckGen(numDecks=40000, chunkSize=10000, filename='decktest_deck4')
     y = loadDeck('data/decktest_decks')
-    w = Parser(y)
+    w = Parser(y, bits=4)
     res = w.rawOut()
     print(pd.DataFrame.from_dict(res).sort_index().sort_index(axis=1))
 
--- src/parser.pyx
+++ src/parser.pyx
@@ -3,32 +3,29 @@ import re
 from typing import Literal, Tuple
 from src.decks import Deck
 from src.fastmatch import winner_counts_for_pair
+from itertools import permutations
 
 class Parser:
 	'''Parse deck list and get scores for each round'''
 	
-	playerOptionsType = Literal['000', '001', '010', '011', '100', '101', '110', '111'] # quick const for player option strings
+	__slots__ = ('decks', 'scores', '_decks_bytes','bits')
 
-	## hardcoded pairs for looping winner
-	PAIRS: list[Tuple[playerOptionsType, playerOptionsType]] = [('000','001'),('000','010'),('000','011'),('000','100'),('000','101'),('000','110'),('000','111'),('001','000'),
-		     ('001','010'),('001','011'),('001','100'),('001','101'),('001','110'),('001','111'),('010','000'),('010','001'),
-		     ('010','011'),('010','100'),('010','101'),('010','110'),('010','111'),('011','000'),('011','001'),('011','010'),
-		     ('011','100'),('011','101'),('011','110'),('011','111'),('100','000'),('100','001'),('100','010'),('100','011'),
-		     ('100','101'),('100','110'),('100','111'),('101','000'),('101','001'),('101','010'),('101','011'),('101','100'),
-		     ('101','110'),('101','111'),('110','000'),('110','001'),('110','010'),('110','011'),('110','100'),('110','101'),
-		     ('110','111'),('111','000'),('111','001'),('111','010'),('111','011'),('111','100'),('111','101'),('111','110')]
-	
-	playerOptions = set(['000', '001', '010', '011', '100', '101', '110', '111'])
-
-	__slots__ = ('decks', 'scores', '_decks_bytes')
-
-	def __init__(self, decks: Deck) -> None:
+	def __init__(self, decks: Deck, bits:Literal[3,4]) -> None:
 		self.decks = decks
 		self.scores = []
+		self.bits = bits
 		self._decks_bytes = [d.encode("ascii") for d in self.decks._decks]
 		return
-	
-	def winner2(self,p1: playerOptionsType , p2: playerOptionsType): # returns a numpy unique counts result 
+
+	@property
+	def playerOptions(self):
+		return set([str(bin(w))[2:].zfill(self.bits) for w in range(2**self.bits)])
+		
+	@property
+	def PAIRS(self):
+		return list(permutations(self.playerOptions,2))
+
+	def winner2(self,p1,p2): # returns a numpy unique counts result 
 		'''Find winner of each trick, return position and winner's choice'''
 
 		def _matcher_str(deck:str, p1, p2):
@@ -59,7 +56,7 @@ class Parser:
 		outcomes = np.unique_counts(np.argmax(winners,axis=1)).counts
 		return outcomes	
 
-	def winner(self, p1: playerOptionsType, p2: playerOptionsType):
+	def winner(self, p1, p2):
 		return winner_counts_for_pair(self._decks_bytes, p1, p2, aligned=False)
 
 	def rawOut(self) -> list:
@@ -71,7 +68,7 @@ class Parser:
 
 	def allPairs(self) -> dict: 
 		'''Output data as a neatly formatted list of lists'''
-		res = {i: {j: (0,0,0) for j in Parser.playerOptions} for i in Parser.playerOptions}
+		res = {i: {j: (0,0,0) for j in self.playerOptions} for i in self.playerOptions}
 		for i,j in self.PAIRS:
 			res[i][j] = self.winner(i,j) # type: ignore
 		return res

e8adab213d04eee352f34556e4885d4daf038432
Theelx <43764914+Theelx@users.noreply.github.com>
2026-03-03 11:50:41 -0500

    pep 8 quotes

--- src/parser.pyx
+++ src/parser.pyx
@@ -2,108 +2,114 @@ import numpy as np
 import re
 from typing import Literal, Tuple
 from src.decks import Deck, deck_gen
+
 try:
-	from src.fastmatch_simd import winner_counts_for_pair
+    from src.fastmatch_simd import winner_counts_for_pair
 except Exception:
-	from src.fastmatch import winner_counts_for_pair
+    from src.fastmatch import winner_counts_for_pair
 from itertools import permutations
 
+
 class Parser:
-	'''Parse deck list and get scores for each round'''
-	
-	__slots__ = ('decks', 'scores', '_decks_bytes', 'bits', "scoring")
+    """Parse deck list and get scores for each round"""
 
-	def __init__(self, decks: Deck, bits:Literal[3,4], scoring_by_tricks: bool = True) -> None:
-		'''Create a parser object for a Deck object
-			
-			params: 
-				decks: Deck object to parse
-				bits: Whether to run the 3 or 4 bit game
-				scoring_by_tricks: Whether or not to score by tricks won or total cards won
+    __slots__ = ("decks", "scores", "_decks_bytes", "bits", "scoring")
 
-		'''
-		self.decks = decks
-		self.scores = []
-		self.bits = bits
-		self._decks_bytes = [d.encode("ascii") for d in self.decks._decks]
-		self.scoring = scoring_by_tricks
-		return
+    def __init__(self, decks: Deck, bits: Literal[3, 4], scoring_by_tricks: bool = True) -> None:
+        """Create a parser object for a Deck object
 
-	@property
-	def playerOptions(self):
-		return set([str(bin(w))[2:].zfill(self.bits) for w in range(2**self.bits)])
-		
-	@property
-	def PAIRS(self):
-		return list(permutations(self.playerOptions,2))
+        params:
+                decks: Deck object to parse
+                bits: Whether to run the 3 or 4 bit game
+                scoring_by_tricks: Whether or not to score by tricks won or total cards won
 
-	def winner2(self,p1,p2): # returns a numpy unique counts result 
-		'''Find winner of each trick, return position and winner's choice'''
+        """
+        self.decks = decks
+        self.scores = []
+        self.bits = bits
+        self._decks_bytes = [d.encode("ascii") for d in self.decks._decks]
+        self.scoring = scoring_by_tricks
+        return
 
-		def _matcher_str(deck:str, p1, p2):
-			'''Handles the actual matching and scoring'''
-			p1score = 0
-			p2score = 0
-			draw = 0
-			newidx = 0
-			cardsLeft = deck
-			while len(cardsLeft) != None:
-				p1match = cardsLeft.find(p1)
-				p2match = cardsLeft.find(p2)
-				if p2match == -1 and p1match == -1: ## no match found
-					draw += 1
-					break
-				elif (p2match < p1match) or (p1match == -1 and p2match > -1): 
-					p2score += p2match + 3 ## index offset to include the cards in the match
-					newidx = p2match + 3
-				elif (p1match < p2match) or (p2match == -1 and p1match > -1):
-					p1score += p1match + 3
-					newidx = p1match + 3
-				cardsLeft = cardsLeft[newidx:] # use rest of deck
-			if p1score == p2score:
-				draw += p2score + p1score + 1
-			return [p1score, p2score, draw]
-		
-		winners = [_matcher_str(w, p1,p2) for w in self.decks._decks]
-		outcomes = np.unique_counts(np.argmax(winners,axis=1)).counts
-		return outcomes
+    @property
+    def playerOptions(self):
+        return set([str(bin(w))[2:].zfill(self.bits) for w in range(2**self.bits)])
 
-	def winner(self, p1, p2) -> list:
-		self.scores = list(winner_counts_for_pair(self._decks_bytes, p1, p2, aligned=False, score_by_tricks=self.scoring))
-		return self.scores
+    @property
+    def PAIRS(self):
+        return list(permutations(self.playerOptions, 2))
 
-	def rawOut(self) -> list:
-		'''Output data as Tuple of str and numpy array'''
-		res = []
-		for i,j in self.PAIRS:
-			w,l,t = self.winner(i,j)
-			res.append([i,j,w,l,t])
-		self.scores = res
-		return res
-	
-	def add_decks(self, deck_count: int, decks=None) -> Parser:
-		"""
-		generate deck_count additional decks (or use provided decks),
-		score them, and update the parser
-		"""
-		if decks is None:
-			new_decks = deck_gen(numDecks=deck_count, save=False)
-		else:
-			if isinstance(decks, Deck):
-				new_decks = decks
-			elif isinstance(decks, list):
-				new_decks = Deck(decks=decks)
-			else:
-				raise TypeError("decks must be a Deck or list[str]")
+    def winner2(self, p1, p2):  # returns a numpy unique counts result
+        """Find winner of each trick, return position and winner's choice"""
 
-		new_bytes = [d.encode("ascii") for d in new_decks._decks]
-		additional_scores = []
-		for i,j in self.PAIRS:
-			additional_scores.append((i,j,winner_counts_for_pair(new_bytes, i, j, aligned=False, score_by_tricks=self.scoring)))
-		for (indx, i), j in zip(enumerate(self.scores.copy()), additional_scores):
-			self.scores[indx] = list(self.scores[indx])
-			self.scores[indx][2] = i[2] + j[2]
-		self.decks._decks = self.decks._decks + new_decks._decks
-		self.decks._deckCount = len(self.decks._decks)
-		self._decks_bytes = self._decks_bytes + new_bytes
-		return self.scores
+        def _matcher_str(deck: str, p1, p2):
+            """Handles the actual matching and scoring"""
+            p1score = 0
+            p2score = 0
+            draw = 0
+            newidx = 0
+            cardsLeft = deck
+            while len(cardsLeft) != None:
+                p1match = cardsLeft.find(p1)
+                p2match = cardsLeft.find(p2)
+                if p2match == -1 and p1match == -1:  ## no match found
+                    draw += 1
+                    break
+                elif (p2match < p1match) or (p1match == -1 and p2match > -1):
+                    p2score += p2match + 3  ## index offset to include the cards in the match
+                    newidx = p2match + 3
+                elif (p1match < p2match) or (p2match == -1 and p1match > -1):
+                    p1score += p1match + 3
+                    newidx = p1match + 3
+                cardsLeft = cardsLeft[newidx:]  # use rest of deck
+            if p1score == p2score:
+                draw += p2score + p1score + 1
+            return [p1score, p2score, draw]
+
+        winners = [_matcher_str(w, p1, p2) for w in self.decks._decks]
+        outcomes = np.unique_counts(np.argmax(winners, axis=1)).counts
+        return outcomes
+
+    def winner(self, p1, p2) -> list:
+        self.scores = list(
+            winner_counts_for_pair(self._decks_bytes, p1, p2, aligned=False, score_by_tricks=self.scoring)
+        )
+        return self.scores
+
+    def rawOut(self) -> list:
+        """Output data as Tuple of str and numpy array"""
+        res = []
+        for i, j in self.PAIRS:
+            w, l, t = self.winner(i, j)
+            res.append([i, j, w, l, t])
+        self.scores = res
+        return res
+
+    def add_decks(self, deck_count: int, decks=None) -> Parser:
+        """
+        generate deck_count additional decks (or use provided decks),
+        score them, and update the parser
+        """
+        if decks is None:
+            new_decks = deck_gen(numDecks=deck_count, save=False)
+        else:
+            if isinstance(decks, Deck):
+                new_decks = decks
+            elif isinstance(decks, list):
+                new_decks = Deck(decks=decks)
+            else:
+                raise TypeError("decks must be a Deck or list[str]")
+
+        new_bytes = [d.encode("ascii") for d in new_decks._decks]
+        additional_scores = []
+        for i, j in self.PAIRS:
+            additional_scores.append(
+                (i, j, winner_counts_for_pair(new_bytes, i, j, aligned=False, score_by_tricks=self.scoring))
+            )
+        for (indx, i), j in zip(enumerate(self.scores.copy()), additional_scores):
+            self.scores[indx] = list(self.scores[indx])
+            self.scores[indx][2] = i[2] + j[2]
+        self.decks._decks = self.decks._decks + new_decks._decks
+        self.decks._deckCount = len(self.decks._decks)
+        self._decks_bytes = self._decks_bytes + new_bytes
+        return self.scores
--- src/scores.py
+++ src/scores.py
@@ -5,28 +5,35 @@ from numpy.typing import NDArray
 from itertools import permutations
 
 
-def load_table(filename:str) -> ScoreTable:
-    w = pd.read_csv(f'scores/{filename}',dtype={'p1choice':str,'p2choice':str}).values.tolist()
+def load_table(filename: str) -> ScoreTable:
+    w = pd.read_csv(f"scores/{filename}", dtype={"p1choice": str, "p2choice": str}).values.tolist()
     return ScoreTable(w)
+
 
 class ScoreTable:
     def __init__(self, data, scoring_by_tricks: bool = True):
         self.scoring = scoring_by_tricks
-        self.raw = pd.DataFrame(data, columns=['p1choice','p2choice','win','loss','tie'])
+        self.raw = pd.DataFrame(data, columns=["p1choice", "p2choice", "win", "loss", "tie"])
         self.table = self._create_table()
 
     def _create_table(self) -> pd.DataFrame:
-        return pd.concat([self.raw[['p1choice','p2choice']],pd.Series(self.raw[['win','loss','tie']].values.tolist(),name='wlt')],axis=1).pivot(columns='p1choice',index='p2choice',values='wlt')
+        return pd.concat(
+            [
+                self.raw[["p1choice", "p2choice"]],
+                pd.Series(self.raw[["win", "loss", "tie"]].values.tolist(), name="wlt"),
+            ],
+            axis=1,
+        ).pivot(columns="p1choice", index="p2choice", values="wlt")
 
     def save(self, filename: str) -> None:
-        '''Save score table as csv file'''
-        self.raw.to_csv(f'scores/{filename}', na_rep='NaN', index=False)
-
+        """Save score table as csv file"""
+        self.raw.to_csv(f"scores/{filename}", na_rep="NaN", index=False)
 
     def __eq__(self, other) -> bool:
         if type(other) == ScoreTable:
             return pd.DataFrame.equals(self.raw, other.raw)
-        else: return False
+        else:
+            return False
 
     def __repr__(self) -> str:
         return self.table.to_string()
